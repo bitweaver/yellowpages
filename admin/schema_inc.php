b67770432dc52b50c3e2b985b05baf612b50f3ff
wjames5 <will@tekimaki.com>
2007-02-11 21:49:25 +0000

    adds defaults, inserts, preference, permisions

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -57,46 +57,57 @@ foreach( array_keys( $tables ) AS $tableName ) {
 }
 
 $gBitInstaller->registerPackageInfo( YELLOWPAGES_PKG_NAME, array(
-	'description' => "YellowPages package to demonstrate how to build a bitweaver package.",
+	'description' => "YellowPages package manages listings.",
 	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>',
 	'version' => '0.1',
-	'state' => 'beta',
+	'state' => 'alpha',
 	'dependencies' => '',
 ) );
 
 // ### Indexes
 $indices = array(
-	'bit_yellowpagess_yellowpages_id_idx' => array('table' => 'bit_yellowpagess', 'cols' => 'yellowpages_id', 'opts' => NULL ),
+	'yellowpages_yellowpages_id_idx' => array('table' => 'yellowpages', 'cols' => 'yellowpages_id', 'opts' => NULL ),
+	'yellowpages_group_id_idx' => array('table' => 'yellowpages_groups', 'cols' => 'group_id', 'opts' => NULL ),
 );
 $gBitInstaller->registerSchemaIndexes( YELLOWPAGES_PKG_NAME, $indices );
 
-/*// ### Sequences
+// ### Sequences
 $sequences = array (
-	'bit_yellowpages_id_seq' => array( 'start' => 1 )
+	'yellowpages_id_seq' => array( 'start' => 1 ),
+	'yellowpages_group_id_seq' => array( 'start' => 1 )
+	'yellowpages_day_id_seq' => array( 'start' => 100 )
 );
 $gBitInstaller->registerSchemaSequences( YELLOWPAGES_PKG_NAME, $sequences );
-*/
+
 
 
 $gBitInstaller->registerSchemaDefault( YELLOWPAGES_PKG_NAME, array(
-	//      "INSERT INTO `".BIT_DB_PREFIX."bit_yellowpages_types` (`type`) VALUES ('YellowPages')",
+	"INSERT INTO `".BIT_DB_PREFIX."yellowpages_days` (`day_id`, `day_title`) VALUES (  0, 'Monday') ",
+	"INSERT INTO `".BIT_DB_PREFIX."yellowpages_days` (`day_id`, `day_title`) VALUES (  1, 'Tuesday') ",
+	"INSERT INTO `".BIT_DB_PREFIX."yellowpages_days` (`day_id`, `day_title`) VALUES (  2, 'Wednesday') ",
+	"INSERT INTO `".BIT_DB_PREFIX."yellowpages_days` (`day_id`, `day_title`) VALUES (  3, 'Thursday') ",
+	"INSERT INTO `".BIT_DB_PREFIX."yellowpages_days` (`day_id`, `day_title`) VALUES (  4, 'Friday') ",
+	"INSERT INTO `".BIT_DB_PREFIX."yellowpages_days` (`day_id`, `day_title`) VALUES (  5, 'Saturday') ",
+	"INSERT INTO `".BIT_DB_PREFIX."yellowpages_days` (`day_id`, `day_title`) VALUES (  6, 'Sunday') ",
+	"INSERT INTO `".BIT_DB_PREFIX."yellowpages_days` (`day_id`, `day_title`) VALUES (  7, 'Holidays') "
 ) );
 
 // ### Default UserPermissions
 $gBitInstaller->registerUserPermissions( YELLOWPAGES_PKG_NAME, array(
-	array( 'bit_p_admin_yellowpages', 'Can admin yellowpages', 'admin', YELLOWPAGES_PKG_NAME ),
-	array( 'bit_p_create_yellowpages', 'Can create a yellowpages', 'registered', YELLOWPAGES_PKG_NAME ),
-	array( 'bit_p_edit_yellowpages', 'Can edit any yellowpages', 'editors', YELLOWPAGES_PKG_NAME ),
-	array( 'bit_p_read_yellowpages', 'Can read yellowpages', 'basic',  YELLOWPAGES_PKG_NAME ),
-	array( 'bit_p_remove_yellowpages', 'Can delete yellowpages', 'admin',  YELLOWPAGES_PKG_NAME ),
+	array( 'p_admin_yellowpages', 'Can admin yellowpages', 'admin', YELLOWPAGES_PKG_NAME ),
+	array( 'p_create_yellowpages', 'Can create a yellowpages', 'registered', YELLOWPAGES_PKG_NAME ),
+	array( 'p_edit_yellowpages', 'Can edit any yellowpages', 'editors', YELLOWPAGES_PKG_NAME ),
+	array( 'p_read_yellowpages', 'Can read yellowpages', 'basic',  YELLOWPAGES_PKG_NAME ),
+	array( 'p_remove_yellowpages', 'Can delete yellowpages', 'admin',  YELLOWPAGES_PKG_NAME ),
 ) );
 
 // ### Default Preferences
 $gBitInstaller->registerPreferences( YELLOWPAGES_PKG_NAME, array(
-	array( YELLOWPAGES_PKG_NAME, 'yellowpages_default_ordering', 'yellowpages_id_desc' ),
 	array( YELLOWPAGES_PKG_NAME, 'yellowpages_list_yellowpages_id', 'y' ),
 	array( YELLOWPAGES_PKG_NAME, 'yellowpages_list_title', 'y' ),
-	array( YELLOWPAGES_PKG_NAME, 'yellowpages_list_description', 'y' ),
-	array( YELLOWPAGES_PKG_NAME, 'feature_listYellowPagess', 'y' ),
+	array( YELLOWPAGES_PKG_NAME, 'yellowpages_list_region', 'y' ),
+	array( YELLOWPAGES_PKG_NAME, 'yellowpages_list_country', 'y' ),
+	array( YELLOWPAGES_PKG_NAME, 'yellowpages_max_list','30'),
+	array( YELLOWPAGES_PKG_NAME, 'yellowpages_rankings','y'),
 ) );
 ?>
