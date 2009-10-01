2e3449302fa357c35079211eec5b032f1a3d5492
wjames5 <will@tekimaki.com>
2009-10-01 14:17:07 +0000

    remove ref to non-existant copyright.txt file

--- admin/admin_yellowpages_inc.php
+++ admin/admin_yellowpages_inc.php
@@ -1,7 +1,7 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_yellowpages/admin/admin_yellowpages_inc.php,v 1.4 2009/10/01 13:45:55 wjames5 Exp $
+// $Header: /cvsroot/bitweaver/_bit_yellowpages/admin/admin_yellowpages_inc.php,v 1.5 2009/10/01 14:17:07 wjames5 Exp $
 // Copyright (c) 2005 bitweaver YellowPages
-// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// All Rights Reserved. See below for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details.
 
 if (isset($_REQUEST["yellowpagesset"]) && isset($_REQUEST["homeYellowPages"])) {
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_yellowpages/index.php,v 1.5 2009/10/01 13:45:55 wjames5 Exp $
+// $Header: /cvsroot/bitweaver/_bit_yellowpages/index.php,v 1.6 2009/10/01 14:17:07 wjames5 Exp $
 // Copyright (c) 2004 bitweaver YellowPages
-// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// All Rights Reserved. See below for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details.
 
 // Initialization
